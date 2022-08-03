c9976a5507d4a8537758f4400461298f51fc4881
PandaMan <shaefferaelita@gmail.com>
2022-08-03 02:09:24 +0200

    not work

--- main.js
+++ main.js
@@ -16,38 +16,39 @@ function nextfilm(){
 
 }
 
-selected.innerHTML = JSON.stringify(getfilm("550"));
+var edit = getfilm("579741");
+console.log(edit);
+selected.innerHTML = JSON.stringify(edit);
 
 function getfilm(id){
+    var resulta = {};
+    var preview = null;
     var url = baseurl+"movie/"+id+"?language=fr&api_key="+apikey;
-    fetch(url)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        url = baseurl+"movie/"+id+"/watch/providers?language=fr&api_key="+apikey;
-        var preview = null;
-        fetch(url).then((resp2) => resp2.json()).then(function(data2) {
-            for(var i in data2.results){
-                if(data2.results[i].type == "Trailer" && data2.Results[i].site == "YouTube"){
-                    preview = "https://www.youtube.com/watch?v="+data2.results[i].key;
-                    console.log(preview)
-                }
-            }
-        }).catch(function(error2) {
-            return error2;
-        });
-
-        const resulta = {
+    var url2 = baseurl+"movie/"+id+"/videos?language=fr&api_key="+apikey;
+    // ---- get trailer list ---- //
+    fetch(url2).then((resp2) => resp2.json()).then(function(data2) {
+        if(!data2){
+           alert("data empty") ;
+        }
+        for(var i in data2.results) {
+                preview = "https://www.youtube.com/watch?v="+data2.results[i].key
+                console.log(preview)
+                //break;
+        }
+    });
+    // ---- get movie ---- //
+    fetch(url).then((resp) => resp.json()).then(function(data) {
+        resulta = {
+            "lang_title":data.title,
             "original_title":data.original_title,
             "date":data.release_date,
             "img":"",
             "vote":data.vote_average,
             "overview":data.overview,
-            "trailer":preview,
+            "trailer":preview
         };
-        console.log(resulta)
-        return resulta;
-        
-    }).catch(function(error) {
-        return error;
     });
+    if(resulta.lang_title){
+        return resulta;
+    }
 }
