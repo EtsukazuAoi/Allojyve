2f082e8053bf79f86b349169da5495ed33561624
PandaMan <shaefferaelita@gmail.com>
2022-08-04 10:15:43 +0200

    ajout detection click poster

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var selected = document.getElementById("main");
 var apikey = "8738dbf792cba5abde36103dd3bc050e";
 var baseurl = "https://api.themoviedb.org/3/";
 var baseimg = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
-
+var maindiv = "";
+var regex = /[^a-z 0-9]/gi;
 async function api(url) {
     let response = await fetch(url);
     if (response.ok) {
@@ -13,11 +14,19 @@ async function api(url) {
     }
 }
 
-function topfilm(){
+async function topfilm(){
+    var html = "<h2>List des dernier sortie :</h2><div class=\"list\">";
     var list = await api("https://api.themoviedb.org/3/movie/now_playing?api_key="+apikey+"&language=fr&page=1&region=fr");
     for(var i in list.results){
-        
+		var title = list.results[i].title
+        var Otitle = list.results[i].original_title;
+        if(Otitle.search(regex) != -1){
+            Otitle = title;
+        }
+        html += "<div><h3>"+Otitle+"</h3><img src='"+baseimg+list.results[i].poster_path+"' alt='"+Otitle+"' /></div>";
     }
+    html += "</div>";
+    maindiv += html;
 }
 
 function lastfilm(){
@@ -40,10 +49,19 @@ async function getfilm(id){
         "lang_title":filminfo.title,
         "original_title":filminfo.original_title,
         "date":filminfo.release_date,
-        "img":"",
+        "img":filminfo.poster_path,
         "vote":filminfo.vote_average,
         "overview":filminfo.overview,
         "trailer":"https://www.youtube.com/watch?v="+trailer.results[0].key
     };
     return resulta;
 }
+
+//async function main(){
+    var topbar = '<div id="navbar"><img src="./charte graphique/allojyvé_logo.png" alt="logo allojyvé"></div>';
+    maindiv += topbar;
+    topfilm();
+    selected.innerHTML = maindiv;
+//}
+
+//main();
